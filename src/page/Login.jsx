c7cd383b5edd31e615e6f95951361d4f20a0bbdc
dipak-kumawat <dipaksk11@gmail.com>
2025-01-11 15:16:58 +0530

    getting the data of country

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import "./Login.css";
+import Table from "../component/Table";
+
 
 const Login = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [countryData, setCountryData] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("Username:", name, "Password:", password);
     try {
-      const response = await fetch("https://distribution.byteelephants.com/api/auth/login/logout-all-device-web?username=sundar_buscuit_admin_user&password=0&deviceInfo=883487", {
+      const response = await fetch(`https://distribution.byteelephants.com/api/auth/login/logout-all-device-web?username=${name}&password=${password}&deviceInfo=883487`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -17,9 +20,49 @@ const Login = () => {
         body: JSON.stringify({ name, password }),
       });
       const data = await response.json();
-      console.log("Response from server:", data);
+      console.log("Login Response Data:", data);
+      const token = data?.externalToken;
+      console.log("Token:", token);
+
+      if (token) {
+        localStorage.setItem("token", token);
+        console.log("Token saved to local storage");
+        fetchCountryData(token);
+      } else {
+        console.error("Token not found in the response!");
+      }
     } catch (error) {
-      console.error("Error:", error);
+      console.error("Error during login:", error);
+    }
+  };
+
+  const fetchCountryData = async () => {
+    console.log("Fetching country data...");
+    try {
+      const token = localStorage.getItem("token");
+      console.log("Token from local storage:", token);
+      if (!token) {
+        console.error("Token not found in local storage");
+        return;
+      }
+      const response = await fetch("https://distribution.byteelephants.com/api/distributiondevelop/v1/country", {
+        method: "GET",
+        headers: {
+          "Authorization":  token,
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setCountryData(data); 
+        console.log("Country Data:", data);
+      } else {
+        const errorMessage = await response.text();
+        console.error("Error fetching countretchiny data:", errorMessage);
+      }
+    } catch (error) {
+      setError("Error fetching country data");
+      console.error("Error fetching country data:", error);
     }
   };
 
@@ -31,7 +74,7 @@ const Login = () => {
           <label htmlFor="name">Username</label>
           <input
             type="text"
-            id="text"
+            id="name"
             placeholder="Enter your name"
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -51,6 +94,14 @@ const Login = () => {
           Login
         </button>
       </form>
+
+
+
+
+    {/* <Table /> */}
+
+
+
     </div>
   );
 };
